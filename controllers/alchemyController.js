225ea839829b6ad68ca3b92f0fda15010c5a04f6
danielvfo <danielvfo@gmail.com>
2017-01-31 03:35:33 -0200

    Created alchemyController.js, fileController.js files and added some start code.

---
+++ controllers/alchemyController.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const fs = require('fs');
+const watson = require('watson-developer-cloud');
+const alchemy_language = watson.alchemy_language({
+  api_key: '296b17b5ca79141c458302bd12be4afed4eb5b5d'
+});
+const jsonfile = require('jsonfile');
+
+module.exports = class alchemyController {
+    constructor() {};
+
+    processFile(callback) {
+        fs.readFile('./Atendimento.csv', function(err, data) {
+            if(err)
+                throw err;
+            callback(data);
+        });
+    };
+
+    callKeyWords(parameters) {
+        alchemy_language.keywords(parameters, function (err, response) {
+            if (err)
+                throw err;
+            else
+                //console.log(JSON.stringify(response, null, 2));
+                fs.writeFile('./temp/data.json', '', function(err) {
+                    if (err)
+                        throw err;
+                });
+                jsonfile.writeFile('./temp/data.json', response, function(err) {
+                    if (err)
+                        throw err;
+                });    
+        });
+    };
+
+    callRelations(parameters) {
+        alchemy_language.relations(parameters, function (err, response) {
+            if (err)
+                console.log('error:', err);
+            else
+                console.log(JSON.stringify(response, null, 2));
+        });
+    };
+
+    callEntities(parameters) {
+        alchemy_language.entities(parameters, function (err, response) {
+            if (err)
+                console.log('error:', err);
+            else
+                console.log(JSON.stringify(response, null, 2));
+        });
+    };        
+
+    callTypedRelations(parameters) {
+        alchemy_language.typedRelations(parameters, function (err, response) {
+            if (err)
+                console.log('error:', err);
+            else
+                console.log(JSON.stringify(response, null, 2));
+        });
+    };
+}
