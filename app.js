7838433735b3e95cf67a1acf3ebaddb8ce4fc58d
danielvfo <danielvfo@gmail.com>
2017-01-31 03:39:26 -0200

    Added some dependencies + test code.

--- app.js
+++ app.js
@@ -21,6 +21,25 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+// require the personalized alchmey module built for this project
+var alchemy = require('./controllers/alchemyController');
+alchemy = new alchemy();
+
+var parameters = {text: ''};
+
+alchemy.processFile(function(data) {
+  //content = data.toString();
+  parameters.text = data.toString();
+  alchemy.callKeyWords(parameters);
+});
+
+
+// processFile(function(data) {
+// content = data.toString();
+// parameters.text = content;
+// //console.log(parameters.text);
+// callAlchemy();
+// });
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
