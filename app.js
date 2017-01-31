feb07a1994c3b06bd4746a751a4f4a338220edad
danielvfo <danielvfo@gmail.com>
2017-01-31 07:31:08 -0200

    Added body-parser  dependency.

--- app.js
+++ app.js
@@ -6,26 +6,24 @@
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
-var express = require('express');
-
+const express = require('express');
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+const cfenv = require('cfenv');
+// Body parser for reading bodies from html requests
+const bodyParser = require('body-parser');
+// Require the personalized alchmey module built for this project
+var alchemy = require('./controllers/alchemyController');
 
 // create a new express server
 var app = express();
-
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
-
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
-
-// require the personalized alchmey module built for this project
-var alchemy = require('./controllers/alchemyController');
-alchemy = new alchemy();
-
 var parameters = {text: ''};
+
+alchemy = new alchemy();
 
 alchemy.processFile(function(data) {
   //content = data.toString();
@@ -40,6 +38,7 @@ alchemy.processFile(function(data) {
 // //console.log(parameters.text);
 // callAlchemy();
 // });
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const fs = require('fs');
+
+module.exports = class fileController {
+    constructor() {}
+
+    processFile(callback) {
+        fs.readFile('./Atendimento.csv', function(err, data) {
+            if(err)
+                throw err;
+            callback(data);
+        });
+    };
+}
