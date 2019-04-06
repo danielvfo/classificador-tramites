ad36d6796021c105f36c0f7a389a9883b6153ccb
Daniel <danielvfo@users.noreply.github.com>
2019-04-06 01:04:54 -0300

    Update alchemyController.js

--- controllers/alchemyController.js
+++ controllers/alchemyController.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const watson = require('watson-developer-cloud');
 const alchemy_language = watson.alchemy_language({
-  api_key: '1983ebb06eafbfc15c6088b77ac1c9b8fe327f43'
+  api_key: ''
 });
 var fileController = require('./fileController');
 fileController = new fileController();
